2f6224825c975b23fd9e996b51f67e82ca2bd060
fomula91 <fomula91@gmail.com>
2022-09-15 20:54:10 +0900

    init vue todo project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <input type="text" v-model="content" />
+    <button type="submit" @click="addTodo">add todolist</button>
+  </div>
+
+  <div id="todolist">
+    <div id="container" v-for="data in todos" :key="data.id">
+      <li id="text">{{ data }}</li>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      todos: [],
+      content: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.content == "") {
+        alert("값을 입력하세요");
+      } else {
+        this.todos.push(this.content);
+        this.content = "";
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
