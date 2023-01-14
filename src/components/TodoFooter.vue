991403b9292f6b184409390e8529c610baeecdc2
fomula91 <fomula91@gmail.com>
2023-01-14 09:26:33 +0900

    feat: footer btn add

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,7 +1,29 @@
-<template lang="">
-  <div>Todo Footer</div>
+<template>
+  <div class="clearAllContainer">
+    <span class="clearAllBtn" v-on:click="clearTodo"> Clear All </span>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    clearTodo: function () {
+      localStorage.clear();
+    },
+  },
+};
 </script>
-<style lang=""></style>
+<style scoped>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background-color: white;
+  border-radius: 5px;
+  margin: 0 auto;
+}
+
+.clearAllBtn {
+  color: #e20303;
+  display: block;
+}
+</style>
